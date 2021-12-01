b515e591b968e9a6b76415177c6a80f4292dda03
SergeiSah <sahhome@yandex.ru>
2021-12-01 15:34:11 +0300

    en_corrector has became to work as file_reader, .py files were concatenated. "corrector" now is the class "LinearCorrector"

--- data_handler.py
+++ data_handler.py
@@ -1,66 +1,67 @@
 import pandas as pd
 from scipy.interpolate import interpolate
-from en_corrector import *
+import plotly.graph_objects as go
 from file_reader import *
 
 
 DATE = "2021.11"
 
 
-def add_to_corrector(corrector: pd.DataFrame, meas_real: tuple) -> pd.DataFrame:
+class LinearCorrector:
 
-    measured_en = meas_real[0]
-    real_en = meas_real[1]
-    correction = meas_real[1] - meas_real[0]
+    def __init__(self, param_1: str, param_2: str):
+        self.corr_df = pd.DataFrame(columns=[param_1, param_2, 'Corr', 'Slope', 'Intercept'])
 
-    to_add_in_corrector = [measured_en, real_en, correction, 0, 0]
-    a_series = pd.Series(to_add_in_corrector, index=corrector.columns)
+    # TODO: Попробовать решить через interpolate или scikit-learn
+    def add_to_corrector(self, meas_real: tuple) -> None:
 
-    if real_en in corrector.En_real.values:
-        corrector_ind = corrector[corrector.En_real == real_en].index[0]
-        corrector.loc[[corrector_ind], :] = to_add_in_corrector
+        measured_en = meas_real[0]
+        real_en = meas_real[1]
+        correction = meas_real[1] - meas_real[0]
 
-        if corrector_ind == 0:    # We cannot calculate coefficients for line with zero index in the corrector
-            return corrector
+        to_add_in_corrector = [measured_en, real_en, correction, 0, 0]
+        a_series = pd.Series(to_add_in_corrector, index=self.corr_df.columns)
 
-        for i in range(corrector_ind, corrector_ind + 2):
-            corrector.loc[[corrector_ind], ['Slope']] = calc_coefficient(slope, corrector, corrector_ind)
-            corrector.loc[[corrector_ind], ['Intercept']] = calc_coefficient(intercept, corrector, corrector_ind)
+        if real_en in self.corr_df.En_real.values:
+            corrector_ind = self.corr_df[self.corr_df.En_real == real_en].index[0]
+            self.corr_df.loc[[corrector_ind], :] = to_add_in_corrector
 
-    else:
-        corrector = corrector.append(a_series, ignore_index=True)
-        last_ind = corrector.shape[0] - 1
-        start_ind = last_ind
+            if corrector_ind != 0:    # We cannot calculate coefficients for line with zero index in the corrector
+                for i in range(corrector_ind, corrector_ind + 2):
+                    self.corr_df.loc[[corrector_ind], ['Slope']] = self.calc_coefficient(self.slope, corrector_ind)
+                    self.corr_df.loc[[corrector_ind], ['Intercept']] = self.calc_coefficient(self.intercept, corrector_ind)
 
-        if corrector.shape[0] > 1:
+        else:
+            self.corr_df = self.corr_df.append(a_series, ignore_index=True)
+            last_ind = self.corr_df.shape[0] - 1
+            start_ind = last_ind
 
-            if corrector.loc[[last_ind], ['En_meas']].values[0] < corrector.loc[[last_ind - 1], ['En_meas']].values[0]:
-                corrector.sort_values(by=['En_meas'], inplace=True)
-                start_ind = 1
+            if self.corr_df.shape[0] > 1:
 
-            for i in range(start_ind, last_ind + 1):
-                corrector.loc[[i], ['Slope']] = calc_coefficient(slope, corrector, last_ind)
-                corrector.loc[[i], ['Intercept']] = calc_coefficient(intercept, corrector, last_ind)
+                if self.corr_df.loc[[last_ind], ['En_meas']].values[0] < self.corr_df.loc[[last_ind - 1], ['En_meas']].values[0]:
+                    self.corr_df.sort_values(by=['En_meas'], inplace=True)
+                    start_ind = 1
 
-    return corrector
+                for i in range(start_ind, last_ind + 1):
+                    self.corr_df.loc[[i], ['Slope']] = self.calc_coefficient(self.slope, last_ind)
+                    self.corr_df.loc[[i], ['Intercept']] = self.calc_coefficient(self.intercept, last_ind)
 
+    def calc_coefficient(self, line_func_coefficient, index):
 
-def calc_coefficient(line_func_coefficient, corrector, index):
+        y1 = self.corr_df.loc[[index], ['Corr']].values[0]
+        y2 = self.corr_df.loc[[index - 1], ['Corr']].values[0]
+        x1 = self.corr_df.loc[[index], ['En_meas']].values[0]
+        x2 = self.corr_df.loc[[index - 1], ['En_meas']].values[0]
 
-    y1 = corrector.loc[[index], ['Corr']].values[0]
-    y2 = corrector.loc[[index - 1], ['Corr']].values[0]
-    x1 = corrector.loc[[index], ['En_meas']].values[0]
-    x2 = corrector.loc[[index - 1], ['En_meas']].values[0]
+        return line_func_coefficient(x1, y1, x2, y2)
 
-    return line_func_coefficient(x1, y1, x2, y2)
+    @staticmethod
+    def slope(x1: float, y1: float, x2: float, y2: float) -> float:
+        return (y1 - y2) / (x1 - x2)
 
-
-def slope(x1: float, y1: float, x2: float, y2: float) -> float:
-    return (y1 - y2) / (x1 - x2)
-
-
-def intercept(x1: float, y1: float, x2: float, y2: float) -> float:
-    return (y1 * x2 - y2 * x1) / (x2 - x1)
+    @staticmethod
+    def intercept(x1: float, y1: float, x2: float, y2: float) -> float:
+        return (y1 * x2 - y2 * x1) / (x2 - x1)
 
 
 def adjust_region(df_to_adjust: pd.DataFrame, df_main: pd.DataFrame) -> pd.DataFrame:
@@ -72,7 +73,7 @@ def adjust_region(df_to_adjust: pd.DataFrame, df_main: pd.DataFrame) -> pd.DataF
     return pd.DataFrame(data={cols[0]: df_main.iloc[:, 0], cols[1]: interp_func(df_main.iloc[:, 0])})
 
 
-en_corrector = pd.DataFrame(columns=['En_meas', 'En_real', 'Corr', 'Slope', 'Intercept'])
+en_corrector = LinearCorrector('En_meas', 'En_real')
 
 
 path_dat_files = f"../{DATE}/Файлы/"
@@ -103,44 +104,50 @@ for ind in df.index:
             continue
 
         # Add data to en_corrector Data Frame
-        en_corrector = add_to_corrector(en_corrector, corr)
+        en_corrector.add_to_corrector(corr)
 
-    elif not pd.isna(Io_file_num):
-        Io_filename = line['First part of file name'].values[0] + str(int(Io_file_num)) + '.dat'
-        Ir_filename = line['First part of file name'].values[0] + str(int(Ir_file_num)) + '.dat'
-        new_filename = str(int(line['file #'].values[0])) + '.dat'
+    # elif not pd.isna(Io_file_num):
+    #     Io_filename = line['First part of file name'].values[0] + str(int(Io_file_num)) + '.dat'
+    #     Ir_filename = line['First part of file name'].values[0] + str(int(Ir_file_num)) + '.dat'
+    #     new_filename = str(int(line['file #'].values[0])) + '.dat'
+    #
+    #     scan_type = line['Scan m. 1'].values[0]
+    #
+    #     Io_data = read_dat_file(path_dat_files + Io_filename, kth_Io, scan_type)
+    #     Ir_data = read_dat_file(path_dat_files + Ir_filename, kth_Ir, scan_type)
+    #
+    #     if (Ir_data is None) or (Io_data is None):
+    #         continue
+    #
+    #     Ir_Io = Ir_data
+    #
+    #     if scan_type == 'en':
+    #         corr_df = en_corrector.corr_df
+    #
+    #         if Ir_data.shape != Io_data.shape:
+    #             Io_data = adjust_region(Io_data, Ir_data)
+    #
+    #         Ir_Io.intensity = Ir_data.intensity / Io_data.intensity * 100
+    #
+    #         # Energy correction
+    #         for corr_ind in corr_df.index:
+    #
+    #             # Find required range
+    #             if Ir_Io.en.mean() < corr_df.loc[[corr_ind], 'En_meas'].values[0]:
+    #                 Ir_Io.en = Ir_Io.en + Ir_Io.en * corr_df.loc[[corr_ind], 'Slope'].values[0] + \
+    #                            corr_df.loc[[corr_ind], 'Intercept'].values[0]
+    #                 break
+    #
+    #     elif scan_type == 'tha':
+    #         Ir_Io.intensity = Ir_data.intensity / Io_data.intensity.mean() * 100
+    #
+    #     Ir_Io.to_csv(path_processed_dat_files + new_filename, sep='\t', index=False, float_format="%.10f")
 
-        scan_type = line['Scan m. 1'].values[0]
+df = en_corrector.corr_df
 
-        # TODO: Interpolate I0 data for x values from Ir data
-        Io_data = read_dat_file(path_dat_files + Io_filename, kth_Io, scan_type)
-        Ir_data = read_dat_file(path_dat_files + Ir_filename, kth_Ir, scan_type)
-
-        if (Ir_data is None) or (Io_data is None):
-            continue
-
-        Ir_Io = Ir_data
-
-        if scan_type == 'en':
-
-            if Ir_data.shape != Io_data.shape:
-                Io_data = adjust_region(Io_data, Ir_data)
-
-            Ir_Io.intensity = Ir_data.intensity / Io_data.intensity * 100
-
-            # Energy correction
-            for corr_ind in en_corrector.index:
-
-                # Find required range
-                if Ir_Io.en.mean() < en_corrector.loc[[corr_ind], 'En_meas'].values[0]:
-                    Ir_Io.en = Ir_Io.en + Ir_Io.en * en_corrector.loc[[corr_ind], 'Slope'].values[0] + \
-                               en_corrector.loc[[corr_ind], 'Intercept'].values[0]
-                    break
-
-        elif scan_type == 'tha':
-            Ir_Io.intensity = Ir_data.intensity / Io_data.intensity.mean() * 100
-
-        Ir_Io.to_csv(path_processed_dat_files + new_filename, sep='\t', index=False, float_format="%.10f")
+fig = go.Figure()
+fig.add_trace(go.Scatter(x=df['En_real'], y=df['Corr']))
+fig.show()
 
 # TODO: Добавить обработку NEXAFS
 # TODO: Автоматически определять kithley (по возможности)
--- file_reader.py
+++ file_reader.py
@@ -1,3 +1,5 @@
+import numpy as np
+from scipy.interpolate import interpolate, pchip_interpolate
 import pandas as pd
 from error_handler import try_read
 
@@ -9,9 +11,42 @@ def read_excel_log(excel_filepath):
 
 
 @try_read
+def en_correction(file_path, real_en, kth_num):
+
+    file = read_dat_file(file_path,
+                         kth_num=kth_num,
+                         scan_type='en')
+    if file is None: return
+
+    file_new = _smoothing(file)
+
+    # file['derv'] = file.intensity.diff() / file.intensity.index.to_series().diff()
+    # file['derv'] = file['derv'] / abs(file['derv'].mean()) * abs(file_new['derv'].mean())
+
+    file_new['derv'] = file_new.intensity.diff() / file_new.intensity.index.to_series().diff()
+    meas_en = file_new[file_new['derv'] == file_new['derv'].min()]['en'].values[0]
+
+    # fig = go.Figure()
+    # fig.add_trace(go.Scatter(x=file_new['en'], y=file_new['derv']))
+    # fig.add_trace(go.Scatter(x=file['en'], y=file['derv']))
+    # fig.show()
+
+    return meas_en, real_en
+
+
+def _smoothing(df, n_scatters=400, window=17):  # Adding points and smoothing
+    energies = np.linspace(min(df['en']), max(df['en']), n_scatters)
+    intensity = interpolate.interp1d(df['en'], df['intensity'], kind='quadratic')
+    step = df['en'][1] - df['en'][0]
+
+    return pd.DataFrame(data={'en': energies + step / 2, 'intensity': intensity(energies)}).rolling(window).mean()
+
+
+@try_read
 def read_dat_file(dat_filepath: str, kth_num: int, scan_type: str) -> pd.DataFrame:
     df = pd.read_csv(dat_filepath, skiprows=85, sep='\s+')
     kth = f'kth{kth_num}'
     new_df = pd.concat([df[df.columns[0]], df[kth] / df['ringc']], axis=1)
 
     return new_df.rename(columns={0: 'intensity', df.columns[0]: scan_type})
+
